e46b8eabe95c6e46c449b201222f1e8efb340d8e
Nick <tietje.n@northeastern.edu>
2025-12-28 11:22:14 -0500

    add empty context menu

--- src/components/notes-page/editor/sidebar-header/sort-menu.tsx
+++ src/components/notes-page/editor/sidebar-header/sort-menu.tsx
@@ -25,6 +25,7 @@ import {
   SortDesc,
 } from '~/lib/icons'
 import { useSortOptions } from '~/hooks/useSortOptions'
+import { EmptyContextMenu } from '~/components/context-menu/components/EmptyContextMenu';
 
 export function SortMenu() {
   const { sortOptions, setSortOptions } = useSortOptions()
@@ -38,58 +39,60 @@ export function SortMenu() {
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        render={<Button variant="ghost" size="icon">
-            <SortDesc className="h-4 w-4" />
-          </Button>
-        }
-      />
-      <DropdownMenuContent align="start">
-        <DropdownMenuRadioGroup
-          value={sortOptions.order}
-          onValueChange={handleSortOrderChange}
-        >
-          <DropdownMenuRadioItem value={SORT_ORDERS.Alphabetical}>
-            {sortOptions.direction === SORT_DIRECTIONS.Ascending ? (
-              <ArrowUpAZ className="mr-2 h-4 w-4" />
-            ) : (
-              <ArrowDownAZ className="mr-2 h-4 w-4" />
-            )}
-            Alphabetical
-          </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value={SORT_ORDERS.DateCreated}>
-            {sortOptions.direction === SORT_DIRECTIONS.Ascending ? (
-              <CalendarArrowUp className="mr-2 h-4 w-4" />
-            ) : (
-              <CalendarArrowDown className="mr-2 h-4 w-4" />
-            )}
-            Date Created
-          </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value={SORT_ORDERS.DateModified}>
-            {sortOptions.direction === SORT_DIRECTIONS.Ascending ? (
-              <ClockArrowUp className="mr-2 h-4 w-4" />
-            ) : (
-              <ClockArrowDown className="mr-2 h-4 w-4" />
-            )}
-            Date Modified
-          </DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup
-          value={sortOptions.direction}
-          onValueChange={handleSortDirectionChange}
-        >
-          <DropdownMenuRadioItem value={SORT_DIRECTIONS.Ascending}>
-            <SortAsc className="mr-2 h-4 w-4" />
-            Ascending
-          </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value={SORT_DIRECTIONS.Descending}>
-            <SortDesc className="mr-2 h-4 w-4" />
-            Descending
-          </DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <EmptyContextMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          render={<Button variant="ghost" size="icon">
+              <SortDesc className="h-4 w-4" />
+            </Button>
+          }
+        />
+        <DropdownMenuContent align="start" className="min-w-48">
+          <DropdownMenuRadioGroup
+            value={sortOptions.order}
+            onValueChange={handleSortOrderChange}
+          >
+            <DropdownMenuRadioItem value={SORT_ORDERS.Alphabetical} className="whitespace-nowrap">
+              {sortOptions.direction === SORT_DIRECTIONS.Ascending ? (
+                <ArrowUpAZ className="mr-2 h-4 w-4" />
+              ) : (
+                <ArrowDownAZ className="mr-2 h-4 w-4" />
+              )}
+              Alphabetical
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value={SORT_ORDERS.DateCreated} className="whitespace-nowrap">
+              {sortOptions.direction === SORT_DIRECTIONS.Ascending ? (
+                <CalendarArrowUp className="mr-2 h-4 w-4" />
+              ) : (
+                <CalendarArrowDown className="mr-2 h-4 w-4" />
+              )}
+              Date Created
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value={SORT_ORDERS.DateModified} className="whitespace-nowrap">
+              {sortOptions.direction === SORT_DIRECTIONS.Ascending ? (
+                <ClockArrowUp className="mr-2 h-4 w-4" />
+              ) : (
+                <ClockArrowDown className="mr-2 h-4 w-4" />
+              )}
+              Date Modified
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup
+            value={sortOptions.direction}
+            onValueChange={handleSortDirectionChange}
+          >
+            <DropdownMenuRadioItem value={SORT_DIRECTIONS.Ascending} className="whitespace-nowrap">
+              <SortAsc className="mr-2 h-4 w-4" />
+              Ascending
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value={SORT_DIRECTIONS.Descending} className="whitespace-nowrap">
+              <SortDesc className="mr-2 h-4 w-4" />
+              Descending
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </EmptyContextMenu>
   )
 }
