1a839c7bcd7967ca946347899da933b829bc534c
Nicholas Tietje <tietje.n@northeastern.edu>
2026-04-13 20:28:31 -0400

    adjust snapshot idle time

--- convex/yjsSync/constants.ts
+++ convex/yjsSync/constants.ts
@@ -1,6 +1,6 @@
 // TTL for awareness state
 export const AWARENESS_TTL_MS = 30 * 1000
 // time AFTER update that a snapshot is made
-export const SNAPSHOT_IDLE_MS = 2 * 60 * 1000
+export const SNAPSHOT_IDLE_MS = 10 * 1000
 // minimum time between snapshots
 export const SNAPSHOT_MIN_INTERVAL_MS = 5 * 60 * 1000
